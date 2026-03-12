b4bc1cd9c40c183319deedf0fad3e91b29e7598c
RosaleeCastro <rosalee.castrot@gmail.com>
2026-03-12 20:14:03 +0100

    add carpeta de ejerciccio restaurant

---
+++ 004-programación_colecciones_fx_obj_definidos_usuario/003-Matrices_arrays/Ejercicio-restaurant/app.js
@@ -0,0 +1,23 @@
+//--------Colas-------//
+let colaEspera = [];
+let colaParrilla = [];
+let colaHorno = [];
+let colaPlancha = [];
+let colaEmplatado = [];
+
+//-------Contorladores
+
+let utltimoId = 0;
+let platosTerminados = 0;
+
+
+//Estaciones ocupadas o libres Booleanos 
+
+let parrillaOcupada = false;
+let hornoOcupado = false;
+let planchOcupada = false;
+let emplatadoOcupado = false;
+
+
+
+
