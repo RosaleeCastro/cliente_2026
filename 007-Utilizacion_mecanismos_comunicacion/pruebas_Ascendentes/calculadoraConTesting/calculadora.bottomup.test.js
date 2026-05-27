ad49f6d1cd4722a8081aaba65b0a7e7038da9628
Rosalee  Lisbett Castro Toribio <rosalee.castrot@gmail.com>
2026-05-27 11:28:00 +0200

    test up

---
+++ 007-Utilizacion_mecanismos_comunicacion/pruebas_Ascendentes/calculadoraConTesting/calculadora.bottomup.test.js
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment jsdom
+ * 
+ * Para instalar jsdom (test al DOM)
+ * npm.cmd install --save-dev jest-environment-jsdom
+ * npm.cmd install --save-dev jest-environment-jsdom
+
+ * Ejecutar los test específicos
+ * npm.cmd run test:integracion
+ */
+const fs = require("fs");
+const path = require("path");
+
